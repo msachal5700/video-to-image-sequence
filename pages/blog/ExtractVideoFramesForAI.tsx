1265e15006958b8da753dda1210782dee0129c1a
Muhammad Sachal <106876496+msachal5700@users.noreply.github.com>
2026-09-21 07:42:22 +0500

    Fix TS2345: useEffect cleanup must return void (wrap removeChild in braces)

--- pages/blog/ExtractVideoFramesForAI.tsx
+++ pages/blog/ExtractVideoFramesForAI.tsx
@@ -23,7 +23,7 @@ const ExtractVideoFramesForAI: React.FC = () => {
       "image": "https://www.videotoimagesequence.online/og-image.png"
     });
     document.head.appendChild(script);
-    return () => document.head.removeChild(script);
+    return () => { document.head.removeChild(script); };
   }, []);
 
   return (
